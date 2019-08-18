913e843dcbed86acfafc0353eb300cb0080e2b62
Leopoldo Teixeira <lmt@cin.ufpe.br>
2019-08-18 17:17:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Tarefa #1 - CALCULADORA 
+
+Esta tarefa envolve os conceitos básicos de projetos Android, UI widgets, e mudanças de configuração. 
+Faça um clone ou fork deste projeto, siga os passos na ordem sugerida e marque mais abaixo, na sua resposta, quais os passos completados. 
+Para entregar o exercício, responda o [formulário de entrega](https://forms.gle/xqVK5c7jkyPQmywm9) até 23/08/2019, às 23h59.
+
+  1. Altere o arquivo de layout `activity_main.xml` para passar a usar `ConstraintLayout` ao invés de `LinearLayout`. Não altere nenhum *id* dos _widgets_;
+  2. Associe listeners para cada botão da calculadora, de forma a inserir a informação no `EditText` com *id* `text_calc`;
+  3. Ao clicar no botão `=`, é feita a avaliação da expressão, armazenada em `text_info`;
+  4. Ao inserir expressões inválidas, exiba um `Toast` ou `Dialog` informando erro, ao invés de quebrar a aplicação; 
+  5. Ao ocorrerem mudanças de configuração, a expressão digitada e o último valor calculado devem permanecer visíveis na aplicação.
+
+---
+
+# Orientações
+
+  - Comente o código que você desenvolver, explicando o que cada parte faz.
+  - Entregue o exercício *mesmo que não tenha completado todos os itens* listados. Marque abaixo apenas o que completou.
+
+----
+
+# Status
+
+| Passo | Completou? |
+| ------ | ------ |
+| 1 | **não** |
+| 2 | **não** |
+| 3 | **não** |
+| 4 | **não** |
+| 5 | **não** |
---
+++ calculadora/app/src/main/java/br/ufpe/cin/android/calculadora/MainActivity.kt
@@ -0,0 +1,106 @@
+package br.ufpe.cin.android.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    //Como usar a função:
+    // eval("2+2") == 4.0
+    // eval("2+3*4") = 14.0
+    // eval("(2+3)*4") = 20.0
+    //Fonte: https://stackoverflow.com/a/26227947
+    fun eval(str: String): Double {
+        return object : Any() {
+            var pos = -1
+            var ch: Char = ' '
+            fun nextChar() {
+                val size = str.length
+                ch = if ((++pos < size)) str.get(pos) else (-1).toChar()
+            }
+
+            fun eat(charToEat: Char): Boolean {
+                while (ch == ' ') nextChar()
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Caractere inesperado: " + ch)
+                return x
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            // | number | functionName factor | factor `^` factor
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'))
+                        x += parseTerm() // adição
+                    else if (eat('-'))
+                        x -= parseTerm() // subtração
+                    else
+                        return x
+                }
+            }
+
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'))
+                        x *= parseFactor() // multiplicação
+                    else if (eat('/'))
+                        x /= parseFactor() // divisão
+                    else
+                        return x
+                }
+            }
+
+            fun parseFactor(): Double {
+                if (eat('+')) return parseFactor() // + unário
+                if (eat('-')) return -parseFactor() // - unário
+                var x: Double
+                val startPos = this.pos
+                if (eat('(')) { // parênteses
+                    x = parseExpression()
+                    eat(')')
+                } else if ((ch in '0'..'9') || ch == '.') { // números
+                    while ((ch in '0'..'9') || ch == '.') nextChar()
+                    x = java.lang.Double.parseDouble(str.substring(startPos, this.pos))
+                } else if (ch in 'a'..'z') { // funções
+                    while (ch in 'a'..'z') nextChar()
+                    val func = str.substring(startPos, this.pos)
+                    x = parseFactor()
+                    if (func == "sqrt")
+                        x = Math.sqrt(x)
+                    else if (func == "sin")
+                        x = Math.sin(Math.toRadians(x))
+                    else if (func == "cos")
+                        x = Math.cos(Math.toRadians(x))
+                    else if (func == "tan")
+                        x = Math.tan(Math.toRadians(x))
+                    else
+                        throw RuntimeException("Função desconhecida: " + func)
+                } else {
+                    throw RuntimeException("Caractere inesperado: " + ch.toChar())
+                }
+                if (eat('^')) x = Math.pow(x, parseFactor()) // potência
+                return x
+            }
+        }.parse()
+    }
+}
